063b444ba0d853ce74b62865980b5353d142953f
alexed34 <alexed34@gmail.com>
2020-06-02 12:02:35 +0400

    22 step add_favikon

--- render_website.py
+++ render_website.py
@@ -1,38 +1,27 @@
-#from http.server import HTTPServer, SimpleHTTPRequestHandler
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json
-from livereload import Server, shell
 import os
-
-server = Server()
-
-def on_reload():
-    template = env.get_template('template.html')
-    for i, part in enumerate(parts, 1):
-        rendered_page = template.render(books=part, pages=pages, index=i)
-        with open(f'pages/index{i}.html', 'w', encoding="utf8") as file:
-            file.write(rendered_page)
-
 
 env = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html'])
 )
 
-
 with open('static/data.json', 'r', encoding='utf8') as f:
     books_json = f.read()
-
-
 
 books = json.loads(books_json)
 parts = [books[i:i + 10] for i in range(0, len(books), 10)]
 pages = len(parts)
+os.makedirs('pages', exist_ok=True)
+template = env.get_template('template.html')
 
-os.makedirs('pages', exist_ok = True)
-on_reload()
+for i, part in enumerate(parts, 1):
+    rendered_page = template.render(books=part, pages=pages, index=i)
 
-server.watch('template.html', on_reload)
-# server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-# server.serve_forever()
-server.serve(root='.')
+    with open(f'pages/index{i}.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+server.serve_forever()
